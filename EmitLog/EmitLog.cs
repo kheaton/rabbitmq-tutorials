22939bd1bebc12af031eba8c41f3c54e7be00c03
Kyle Heaton <kyle.heaton@maritzcx.com>
2018-07-13 10:15:14 -0600

    Completing some tutorials

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace EmitLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() 
+            { 
+                HostName = "localhost", 
+                UserName = "rabbitmq", 
+                Password = "rabbitmq",
+            };
+
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+                    var message = GetMessage(args);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "logs",
+                                         routingKey: string.Empty,
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine($" [x] Send {message}");
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args.Any() ? string.Join(" ", args) : "info: Hello World!";
+        }
+    }
+}
